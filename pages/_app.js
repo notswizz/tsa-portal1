f2bfb414bfc4175156f812bc04857555588d260f
jack smith <emailswizz@gmail.com>
2025-07-08 13:47:58 -0400

    OK

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 }
